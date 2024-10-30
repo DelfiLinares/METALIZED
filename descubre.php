3240127df8be9cb59fbad6193319d2f79aad931e
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2024-10-30 12:03:39 -0300

    Add files via upload

--- descubre.php
+++ descubre.php
@@ -51,7 +51,7 @@
     <style> @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'); </style>
     <link rel="icon" type="image/png" href="calavera.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="metalized.css" type="text/css"/>
+    <link rel="stylesheet" href="Descubre.css" type="text/css"/>
     <title>Metalized</title>
 </head>
 
